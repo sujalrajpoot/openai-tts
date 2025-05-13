ca8d444d5e458e8858940feb149998469176a843
Sujal Rajpoot <104921805+sujalrajpoot@users.noreply.github.com>
2025-05-13 22:30:51 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='1.0.0',
     description='A powerful and easy-to-use Python library for generating natural-sounding speech using OpenAI text-to-speech capabilities.',
     author='Sujal Rajpoot',
+    author_email="sujalrajpoot70@gmail.com",
     packages=find_packages(),
     install_requires=[
         'requests'
